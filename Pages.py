04a823196bcdcb3300f0790673d7c397ba10f94a
Rishi Koneru <rishikoneru2005@gmail.com>
2024-07-16 15:24:32 -0400

    added STalign_Prep class

--- Pages.py
+++ Pages.py
@@ -1,8 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 import os
-from Image import Image
-from Atlas import Atlas
+from Images import *
+
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  
+NavigationToolbar2Tk) 
 
 class Page:
 
@@ -66,7 +69,42 @@ class STalign_Prep(Page):
     def __init__(self, master, atlas_name, target_address):
 
         super().__init__(master)
-        atlas = Atlas(atlas_name)
+        self.atlas = Atlas(atlas_name)
+        self.target = Target(target_address, self.atlas.xA) # TODO: chagne to target class once complete
+        
+        # rotation scale
+        self.rot_scale = ttk.Scale(self.frame, from_=60, to=-60, 
+                                   orient='horizontal', length=200,
+                                   variable=self.atlas.theta_degrees,
+                                   command=self.update) 
+
+        #slice scale
+        self.slice_scale = ttk.Scale(self.frame, from_=0, to=self.atlas.img.shape[0]-1,
+                                     orient='vertical', length=500, 
+                                     variable=self.atlas.curr_slice,
+                                     command=self.update)
+
+        # showing images
+        self.fig = Figure()
+        self.canvas = FigureCanvasTkAgg(self.fig, self.frame)
+        self.fig.subplots(1,2)
+        self.fig.axes[1].imshow(self.target.get_img())
+        self.update()
+        
+        # add mpl toolbar to allow zoom, translation
+        toolbar_frame = ttk.Frame(self.frame)
+        toolbar = NavigationToolbar2Tk(self.canvas, toolbar_frame) 
+        toolbar.update()
+
+        self.rot_scale.grid(row=0, column=1)
+        self.slice_scale.grid(row=1, column=0)
+        self.canvas.get_tk_widget().grid(row=1, column=1)
+        toolbar_frame.grid(row=2, column=1)
+    
+    def update(self, _=None):
+        self.fig.axes[0].imshow(self.atlas.get_img())
+        self.canvas.draw()
+
 
 
     

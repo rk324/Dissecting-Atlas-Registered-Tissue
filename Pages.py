69e492f08cdb22e2621c9517c29d928ca8fdc015
Rishi Koneru <rishikoneru2005@gmail.com>
2024-07-16 11:41:20 -0400

    created STalign class and added next page traversal to Starter class

--- Pages.py
+++ Pages.py
@@ -8,12 +8,17 @@ class Page:
 
     def __init__(self,master):
         self.frame = ttk.Frame(master)
-    
+        self.master = master
+        
     def activate(self):
         self.frame.grid(row=0,columnspan=2)
     
     def deactivate(self):
         self.frame.grid_remove()
+    
+    # just in case gets called in a child class that doesnt havea definition for it
+    def previous(self): pass
+    def next(self): pass
 
 class Starter(Page):
 
@@ -30,7 +35,7 @@ class Starter(Page):
 
         self.target_file_name = tk.StringVar()
         target_picker_label = ttk.Label(self.frame,text="Target:")
-        target_picker_entry = ttk.Entry(self.frame,textvariable=self.target_file_name)
+        target_picker_entry = ttk.Label(self.frame,textvariable=self.target_file_name)
         target_picker_btn = ttk.Button(self.frame,text="Browse",command=self.select_target)
 
         atlas_picker_label.grid(row=0,column=0)
@@ -43,4 +48,25 @@ class Starter(Page):
         file = tk.filedialog.askopenfile(initialdir=os.curdir)
         if file is None: return False # no file selected
         self.target_file_name.set(file.name)
+    
+    def next(self):
+
+        chosen_atlas = self.atlas_name.get()
+        target_address = self.target_file_name.get()
+
+        if chosen_atlas == "Choose Atlas": raise Exception("Please choose an atlas")
+        elif target_address == "": raise Exception("Please select a target image file")
+
+        self.deactivate()
+        return STalign_Prep(self.master, chosen_atlas, target_address)
+
+
+class STalign_Prep(Page):
+    
+    def __init__(self, master, atlas_name, target_address):
+
+        super().__init__(master)
+        atlas = Atlas(atlas_name)
+
+
     

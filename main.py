0455938d8084310ba25466ea32ff82621e5b1e4b
Rishi Koneru <rishikoneru2005@gmail.com>
2024-07-16 11:40:28 -0400

    added next and previous page traversal

--- main.py
+++ main.py
@@ -9,18 +9,33 @@ from abc import ABC
 class App:
     def __init__(self):
         self.root = tk.Tk()
-        self.pages = [Starter(self.root)]
+        self.pages = [Starter(self.root)] # TODO: add more pages
         self.curr_page_idx = 0
         
         self.prev_btn = ttk.Button(self.root,text="Previous")
-        self.nxt_btn = ttk.Button(self.root,text="Next")
+        self.nxt_btn = ttk.Button(self.root,text="Next", command=self.nxt_pg)
+
+    def start(self):
+        self.update()
+        self.root.mainloop()
+    
+    def nxt_pg(self):
+        nxt_pg = self.pages[self.curr_page_idx].next()
+        self.curr_page_idx += 1
+        self.pages.append(nxt_pg)
+        self.update()
+    
+    def update(self):
+        self.pages[self.curr_page_idx].activate()
+        
+        # logic for showing next and previous buttons
         self.prev_btn.grid(row=1,column=0)
         self.nxt_btn.grid(row=1,column=1)
+        if self.curr_page_idx == 0:
+            self.prev_btn.grid_remove()
+        elif self.curr_page_idx == 4:
+            self.nxt_btn.grid_remove()
 
-    
-    def start(self):
-        self.pages[self.curr_page_idx].activate()
-        self.root.mainloop()
         
 
 

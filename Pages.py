9b4ce8ffce6f177542fa3745796e26c2f1b30397
Rishi Koneru <rishikoneru2005@gmail.com>
2024-07-16 10:56:38 -0400

    added page parent class with activate and deactivate methods

--- Pages.py
+++ Pages.py
@@ -4,30 +4,40 @@ import os
 from Image import Image
 from Atlas import Atlas
 
-class Starter:
+class Page:
+
+    def __init__(self,master):
+        self.frame = ttk.Frame(master)
+    
+    def activate(self):
+        self.frame.grid(row=0,columnspan=2)
+    
+    def deactivate(self):
+        self.frame.grid_remove()
+
+class Starter(Page):
 
     def __init__(self, master):
 
+        super().__init__(master)
+
         self.atlas_name = tk.StringVar()
         atlases = [name for name in os.listdir('Data\\Atlases')]
-        atlas_picker_label = ttk.Label(master,text="Atlas:")
-        atlas_picker_combo = ttk.Combobox(master,values=atlases, state='readonly',
+        atlas_picker_label = ttk.Label(self.frame,text="Atlas:")
+        atlas_picker_combo = ttk.Combobox(self.frame,values=atlases, state='readonly',
                                           textvariable=self.atlas_name)
         atlas_picker_combo.set("Choose Atlas")
 
         self.target_file_name = tk.StringVar()
-        target_picker_label = ttk.Label(master,text="Target:")
-        target_picker_entry = ttk.Entry(master,textvariable=self.target_file_name)
-        target_picker_btn = ttk.Button(master,text="Browse",command=self.select_target)
-
-        start_btn = ttk.Button(master,text="Start!")
+        target_picker_label = ttk.Label(self.frame,text="Target:")
+        target_picker_entry = ttk.Entry(self.frame,textvariable=self.target_file_name)
+        target_picker_btn = ttk.Button(self.frame,text="Browse",command=self.select_target)
 
         atlas_picker_label.grid(row=0,column=0)
         atlas_picker_combo.grid(row=0,column=1,)
         target_picker_label.grid(row=1,column=0)
         target_picker_entry.grid(row=1,column=1)
         target_picker_btn.grid(row=1,column=2)
-        start_btn.grid(row=2,columnspan=3)
     
     def select_target(self):
         file = tk.filedialog.askopenfile(initialdir=os.curdir)
--- main.py
+++ main.py
@@ -3,20 +3,26 @@ from tkinter import ttk
 import os
 #from Image import Image
 #from Atlas import Atlas
-from start import Starter
+from Pages import *
+from abc import ABC
 
 class App:
     def __init__(self):
-        root = tk.Tk()
-        curr_page = Starter(root)
-        root.mainloop()
+        self.root = tk.Tk()
+        self.pages = [Starter(self.root)]
+        self.curr_page_idx = 0
+        
+        self.prev_btn = ttk.Button(self.root,text="Previous")
+        self.nxt_btn = ttk.Button(self.root,text="Next")
+        self.prev_btn.grid(row=1,column=0)
+        self.nxt_btn.grid(row=1,column=1)
+
+    
+    def start(self):
+        self.pages[self.curr_page_idx].activate()
+        self.root.mainloop()
+        
 
 
 app = App()
-        
-
-'''img = Atlas(root)
-img.frame.pack(side=tk.LEFT)
-img2 = Image(root)
-img2.frame.pack(side=tk.LEFT)
-'''
+app.start()

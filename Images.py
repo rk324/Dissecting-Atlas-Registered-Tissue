8214b08f687e301e4446477e83912600ef0d0779
Rishi Koneru <rishikoneru2005@gmail.com>
2024-07-17 16:10:45 -0400

    Landmark annotator functionality

--- Images.py
+++ Images.py
@@ -14,28 +14,42 @@ from matplotlib.animation import FuncAnimation
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,  
 NavigationToolbar2Tk)
 
-class Atlas():
+class Image():
+
+    def __init__(self, img_name): 
+        self.load(img_name)
+        self.pix_loc = [np.arange(n)*d - (n-1)*d/2.0 for n,d in zip(self.img.shape,self.pix_dim)]
+    
+    def load(self, img_name):
+        pass
+
+    def get_img(self): return self.img
+    def get_extent(self):
+        return STalign.extent_from_x(self.pix_loc[-1:-3:-1][::-1])
+
+class Atlas(Image):
     
     def __init__(self, atlas_name):
+
+        super().__init__(atlas_name)
 
         self.curr_slice = tk.IntVar()
         self.theta_degrees = tk.IntVar()
 
+        self.curr_slice.set(int(self.img.shape[0]/2))
+        self.prev_theta = 0
+        self.cache_img = self.img[self.curr_slice.get()]
+
+    def load(self, atlas_name):
         # get img and segmentation from folder
         path = f'Data\\Atlases\\{atlas_name}'
         img_list = [f'{path}\\{name}' for name in os.listdir(path)]
         filetype = img_list[0][img_list[0].index('.')+1:] # TODO: find a better way to get file extension
 
         # file opening based on filetype
-        if filetype == 'nii': dxA = self.load_nii(img_list)
-        elif filetype == 'nrrd': dxA = self.load_nrrd(img_list)
+        if filetype == 'nii': self.load_nii(img_list)
+        elif filetype == 'nrrd': self.load_nrrd(img_list)
         else: raise Exception ("Invalid atlas file type!")
-
-        self.prev_theta = self.theta_degrees.get()
-        self.cache_img = self.img[self.curr_slice.get()]
-
-        self.curr_slice.set(int(self.img.shape[0]/2))
-        self.xA = [np.arange(n)*d - (n-1)*d/2.0 for n,d in zip(self.img.shape,dxA)]
  
     def load_nii(self, img_list):
         img = nib.load(img_list[0])
@@ -50,19 +64,17 @@ class Atlas():
         if img.header['xyzt_units'] < 1 or img.header['xyzt_units'] > 3:
             raise Exception("Error: atlas not well formatted")
         pix_multi = 1000**(3-img.header['xyzt_units'])
-        dxA = np.roll(img.header['pixdim'][1:4],2)*pix_multi
-        return dxA
+        self.pix_dim = np.roll(img.header['pixdim'][1:4],2)*pix_multi
     
     def load_nrrd(self, img_list):
         self.img,_ = nrrd.read(img_list[0])
         self.labels,hdr = nrrd.read(img_list[1])
 
-        dxA = np.diag(hdr['space directions'])
-        return dxA
+        self.pix_dim = np.diag(hdr['space directions'])
 
-    def get_img(self):
+    def get_img(self, theta=None):
 
-        theta = self.theta_degrees.get()
+        if theta is None: theta = self.theta_degrees.get()
         rot_diff = theta-self.prev_theta
         if rot_diff == 0: # changing slice, no reason to use cached img
             return rotate(self.img[self.curr_slice.get()], theta)
@@ -73,18 +85,19 @@ class Atlas():
                                     theta)
         return self.cache_img
         
-class Target: 
+class Target(Image): 
 
-    def __init__(self, img_name,xA):
-        W = ski.color.rgb2gray(ski.io.imread(img_name))
-        atlas_dim = [len(x) for x in xA]
-        dxA = [x[1]-x[0] for x in xA]
+    def __init__(self, filename, src):
         
+        self.src_atlas = src
+        super().__init__(filename)
+
+    def load(self, filename):
+        
+        self.pix_dim = self.src_atlas.pix_dim[1:]
         # TODO: rescaling/downscaling image + padding based on how 
         # much of img is the actual slice
+        W = ski.color.rgb2gray(ski.io.imread(filename))
         W = (W - np.min(W)) / (np.max(W) - np.min(W)) # normalizing img
         # TODO: add feature to invert colors
         self.img = W
-    
-    def get_img(self):
-        return self.img
--- Pages.py
+++ Pages.py
@@ -70,7 +70,7 @@ class STalign_Prep(Page):
 
         super().__init__(master)
         self.atlas = Atlas(atlas_name)
-        self.target = Target(target_address, self.atlas.xA) # TODO: chagne to target class once complete
+        self.target = Target(target_address, self.atlas)
         
         # rotation scale
         self.rot_scale = ttk.Scale(self.frame, from_=60, to=-60, 
@@ -112,9 +112,119 @@ class STalign_Prep(Page):
 class Landmark_Annotator(Page):
 
     def __init__(self, master, atlas, target):
+        super().__init__(master)
+
         self.atlas = atlas
         self.target = target
+    
+        self.imgs = [atlas.get_img(0), target.get_img()]
+        self.extents = [atlas.get_extent(), target.get_extent()]
+        self.points = [ [], [] ] # landmark points, points[0][i] in atlas corresponds with points[1][i] in target
+        self.new_pt = [ [], [] ]
+        self.pt_sz = 2
+        
+        # controls
+        text = '''Mark atlas-target point pairs one pair at a time and submit each pair before marking the next! Click 'Next' to move on
+        
+        Controls:
+        L-click\tmark point
+        R-click\tremove point
+        Enter\tsubmit point
+        Backspace\tdelete last submitted'''
+        instructions = ttk.Label(self.frame, text=text)
 
+        # showing images
+        self.fig = Figure()
+        self.canvas = FigureCanvasTkAgg(self.fig, self.frame)
+        self.fig.subplots(1,2)
+        self.update()
+        
+        # add mpl toolbar to allow zoom, translation
+        toolbar_frame = ttk.Frame(self.frame)
+        toolbar = NavigationToolbar2Tk(self.canvas, toolbar_frame) 
+        toolbar.update()
+
+        instructions.grid(row=0, column=0)
+        self.canvas.get_tk_widget().grid(row=1, column=0)
+        toolbar_frame.grid(row=2, column=0)
+
+        self.canvas.mpl_connect('button_press_event', self.onclick)
+        self.canvas.mpl_connect('key_press_event',self.onpress)
+
+    def update(self, axis=None):
+
+        if axis is None:
+            self.update(0)
+            self.update(1)
+            return
+        elif axis not in [0,1]: 
+            raise Exception(f"Bad call to Landmark_Annotator.update(axis={axis})")
+
+        self.fig.axes[axis].cla()
+        self.fig.axes[axis].imshow(self.imgs[axis], extent=self.extents[axis])
+        
+        # plot prev selected points in white
+        if len(self.points[0]): 
+            self.fig.axes[axis].scatter(np.array(self.points)[axis,:,2],
+                                        np.array(self.points)[axis,:,1], 
+                                        color='white', s=self.pt_sz)
+        
+        self.canvas.draw()
+    
+    def onclick(self, event):
+        if event.xdata == None: return # clicked outside of axes
+        ix, iy = int(event.xdata), int(event.ydata) # get x and y data of pt
+        iz = 0
+        msg = ''
+        
+        axis = -1
+        # based on where user clicked, set axis and update output msg
+        if event.inaxes == self.fig.axes[0]: # user clicked atlas
+            axis = 0
+            iz = self.atlas.pix_loc[0][self.atlas.curr_slice.get()] # z value corresponding to slice
+            msg = "source at " + msg
+        elif event.inaxes == self.fig.axes[1]: # user clicked target
+            axis = 1
+            msg = "target at " + msg
+        else: print("ERROR: clicked outside axes")
+        
+        # update that axis to clear out uncomitted pts
+        self.update(axis)
+
+        if event.button == 1: # left click means add point at mouse location
+            self.fig.axes[axis].scatter([ix],[iy], color='red', s=self.pt_sz)
+            self.new_pt[axis] = [iz, iy, ix]
+            msg = 'point added to ' + msg + f'[x,y]=[{ix},{iy}]'
+        elif event.button == 3: # right click means remove previously created point
+            msg = 'point removed from ' + msg + f'[x,y]={self.new_pt[axis][2:0:-1]}'
+            self.new_pt[axis] = []
+        
+        print(msg)
+        self.canvas.draw()
+
+    def onpress(self, event):
+        if event.key == 'enter': # enter key used to commit selected points to points list
+
+            if not len(self.new_pt[0])*len(self.new_pt[1]): # if missing a point in either axis, throw error
+                print("ERROR: attempted landmark save with one or more points missing!")
+                return
+
+            # add new points to list, notify user, and clear out new points list
+            self.points[0].append(self.new_pt[0])
+            self.points[1].append(self.new_pt[1])
+            print(f"Added {self.new_pt[0][2:0:-1]} and {self.new_pt[1][2:0:-1]} to points list")
+            self.new_pt[0] = []
+            self.new_pt[1] = []
+            self.update()
+        
+        if event.key == 'backspace': # backspace key used to remove recently committed point
+            if len(self.points[0]) == 0: return # if no points to remove, simply return
+            print(f'Removed [{self.points[0][-1][2:0:-1]}] and [{self.points[1][-1][2:0:-1]}]') # user msg
+            
+            # remove last pair of poins
+            self.points[0].pop(-1)
+            self.points[1].pop(-1)
+            self.update() # refresh both axes
 
 
 

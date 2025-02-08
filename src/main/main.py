7c50d15393581910038c041463fa9b2c87fe26c2
Rishi <rishikoneru2005@gmail.com>
2025-02-08 12:47:27 -0500

    restructuring App as a child of tk.Tk

--- src/main/main.py
+++ src/main/main.py
@@ -6,66 +6,70 @@ import os
 from Pages import *
 from abc import ABC
 
-class App:
+class App(tk.Tk):
     def __init__(self):
-        self.root = tk.Tk()
-        self.pages = [Starter(self.root)]
-        self.curr_page_idx = 0
-        
-        self.pg_header = tk.StringVar()
-        pg_header_label = ttk.Label(self.root,textvariable=self.pg_header)
+        # initializes app with main window, navigation bar, prev/next buttons in nav_bar, 
+        super().__init__()
+        self.create_widgets()
+        self.show_widgets()
 
-        btns_frame = ttk.Frame(self.root)
-        self.prev_txt = tk.StringVar(value='Previous')
-        self.nxt_txt = tk.StringVar(value='Next')
-        self.prev_btn = ttk.Button(btns_frame,textvariable=self.prev_txt, command=self.prev_pg)
-        self.nxt_btn = ttk.Button(btns_frame,textvariable=self.nxt_txt, command=self.nxt_pg)
+        # TODO: template Slide class, adjust Atlas class
 
-        pg_header_label.grid(row=0, column=0)
-        btns_frame.grid(row=2, column=0,sticky='we',padx=10, pady=5)
+        self.slides: list[Slide] = []
+        self.atlas = Atlas()
+
+        page_list: tuple[Page] = (Starter,STalign_Prep)
+        self.pages: list[Page] = [page(self.main_window, self.slides, self.atlas) for page in page_list] # initalize each page in here with self.main_window as parent
+        self.page_index = 0
+
+        self.mainloop()
+
+
+    def create_widgets(self):
+        self.main_window = tk.Frame(self)
+        self.nav_bar = tk.Frame(self)
+        self.prev_btn = ttk.Button(self.nav_bar, command=self.prev_page)
+        self.next_btn = ttk.Button(self.nav_bar, command=self.next_page)
+
+    def show_widgets(self):
+        # show nav_bar and main_window
+        self.main_window.grid(row=1, column=0, sticky='we')
+        self.nav_bar.grid(row=2, column=0,sticky='se', padx=10, pady=5)
         self.prev_btn.pack(side=tk.LEFT)
-        self.nxt_btn.pack(side=tk.RIGHT)
-
-    def start(self):
-        self.update()
-        self.root.mainloop()
+        self.next_btn.pack(side=tk.RIGHT)
     
-    def nxt_pg(self):
-        nxt_pg = self.pages[self.curr_page_idx].next()
-        if self.curr_page_idx == 4:
-            self.root.destroy()
+    def next_page(self):
+        if self.page_index == len(self.pages)-1:
+            self.destroy()
             return
-        self.curr_page_idx += 1
-        self.pages.append(nxt_pg)
+
+        self.pages[self.page_index].deactive()
+        self.page_index += 1
         self.update()
 
-    def prev_pg(self):
+    def prev_page(self):
 
-        if self.curr_page_idx == 0: 
-            self.root.destroy()
+        if self.page_index == 0: 
+            self.destroy()
             return
-
-        self.pages[self.curr_page_idx].frame.destroy()
-        self.pages.pop(self.curr_page_idx)
-        self.curr_page_idx -= 1
+        
+        self.pages[self.page_index].deactive()
+        self.page_index -= 1
         self.update()
     
     def update(self):
         # activate current page
-        self.pages[self.curr_page_idx].activate()
-        
-        # show page header for current page
-        self.pg_header.set(self.pages[self.curr_page_idx].header)
+        #self.pages[self.page_index].activate()
 
         # logic for showing next and previous buttons
-        self.prev_txt.set('Previous')
-        self.nxt_txt.set('Next')
-        if self.curr_page_idx == 0:
-            self.prev_txt.set('Exit')
-        elif self.curr_page_idx == 4:
-            self.nxt_txt.set('Finish')
+        self.prev_btn.config(text='Previous')
+        self.next_btn.config(text='Next')
+        if self.page_index == 0:
+            self.prev_btn.config(text='Exit')
+        elif self.page_index == len(self.pages)-1:
+            self.next_btn.config(text='Finish')
 
 
 # Actually run the app
 app = App()
-app.start()
+#app.start()

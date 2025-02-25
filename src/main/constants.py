f129b997bbfc082fb24a0b732e2a3988900bae0b
Rishi <rishikoneru2005@gmail.com>
2025-02-25 00:42:34 -0500

    added default stalign params to constants

--- src/main/constants.py
+++ src/main/constants.py
@@ -2,3 +2,12 @@ FSR = "full size reference"
 DSR = "downscale reference"
 FSL = "full size label"
 DSL = "downscale label"
+
+DEFAULT_STALIGN_PARAMS = {
+            'timesteps': 12,
+            'iterations': 100,
+            'sigmaM': 0.5,
+            'sigmaP': 1,
+            'sigmaR': 1e8,
+            'resolution': 250
+        }

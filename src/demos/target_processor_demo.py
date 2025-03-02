cbe90594f95bb1bfda457189998fa2ea3ff907b4
Rishi <rishikoneru2005@gmail.com>
2025-03-01 20:51:03 -0500

    created target processor demo

---
+++ src/demos/target_processor_demo.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter import ttk
+import sys
+import os
+sys.path.append(os.path.join('src','main'))
+
+from images import Slide, Atlas
+from constants import FSR, DSR, FSL, DSL
+from pages import Starter, SlideProcessor, TargetProcessor
+print('hello')
+
+root = tk.Tk()
+slides: list[Slide] = []
+atlases = {
+    FSR: Atlas(),
+    DSR: Atlas(),
+    FSL: Atlas(),
+    DSL: Atlas(),
+    'names': None
+}
+
+starter = Starter(root, slides, atlases)
+starter.load_atlas_info(os.path.join('atlases','allen_nissl_100um'))
+starter.load_slides('demo_images')
+slide_processor = SlideProcessor(root, slides, atlases)
+
+demo = TargetProcessor(root, slides, atlases)
+demo.activate()
+root.mainloop()

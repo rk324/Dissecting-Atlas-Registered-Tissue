e22227c090ffc192d480783b99ef074738e80c31
Rishi <rishikoneru2005@gmail.com>
2025-03-04 11:39:39 -0500

    deleted commented out code

--- src/demos/slide_processor_demo.py
+++ src/demos/slide_processor_demo.py
@@ -12,10 +12,3 @@ class SlideProcessorDemo(Demo):
 
 demo = SlideProcessorDemo()
 demo.run()
-'''
-starter = Starter(root, slides, atlases)
-starter.load_atlas_info(os.path.join('atlases','allen_nissl_100um'))
-starter.load_slides('demo_images')
-demo = SlideProcessor(root, slides, atlases)
-demo.activate()
-root.mainloop()'''

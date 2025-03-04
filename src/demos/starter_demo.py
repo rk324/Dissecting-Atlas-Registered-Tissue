6c412611e1ebfdac7a20ee123ef548bf55a4103e
Rishi <rishikoneru2005@gmail.com>
2025-03-04 11:38:13 -0500

    added checkpoint name property

--- src/demos/starter_demo.py
+++ src/demos/starter_demo.py
@@ -6,9 +6,8 @@ class StarterDemo(Demo):
     def __init__(self):
         super().__init__()
         self.demo_widget = Starter(self.widget_frame, self.slides, self.atlases)
+        self.checkpoint_name = "post_starter.pkl"
 
-    def done(self):
-        super().done()
 
 demo = StarterDemo()
 demo.run()

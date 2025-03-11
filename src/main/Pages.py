8c7bfe4616f4963a9898fde42578e40c7cef8804
Rishi <rishikoneru2005@gmail.com>
2025-03-11 09:50:22 -0400

    completed TargetProcessor, deleted STalignPrep and LandmarkAnnotator

--- src/main/Pages.py
+++ src/main/Pages.py
@@ -6,6 +6,7 @@ from constants import *
 import torch
 import shapely
 import pandas as pd
+import numpy as np
 from sklearn.cluster import dbscan
 
 import matplotlib as mpl
@@ -776,6 +777,16 @@ class TargetProcessor(Page):
         self.target_nav_combo.config(
             values=[i+1 for i in range(self.currSlide.numTargets)]
         )
+
+        if (self.currTarget.thetas == np.array([0,0,0])).all():
+            n = 0
+            thetas_avg = np.array([0,0,0])
+            for target in self.currSlide.targets:
+                if (target.thetas != np.array([0,0,0])).any():
+                    thetas_avg += target.thetas
+                    n += 1
+            if n > 0: self.currTarget.thetas = np.divide(thetas_avg,n).astype('int64')
+
         for i in range(3): self.thetas[i].set(self.currTarget.thetas[i])
         self.translation.set(self.currTarget.T_estim[0])
 
@@ -848,7 +859,7 @@ class TargetProcessor(Page):
 
         self.update_img_estim(self.currTarget)
         self.slice_viewer.axes[1].imshow(self.currTarget.img_estim.get_img(), cmap='Grays')
-        
+
         point = self.new_points[1]
         if len(point) == 2: 
             self.slice_viewer.axes[1].scatter(
@@ -925,6 +936,8 @@ class TargetProcessor(Page):
         # estimate pixel dimensions
         for slide in self.slides:
             slide.estimate_pix_dim()
+
+        # process points
         super().done()
 
     def cancel(self):
@@ -942,195 +955,7 @@ class TargetProcessor(Page):
     def get_target_index(self):
         return self.curr_target_var.get()-1
 
-class STalign_Prep(Page):
-    
-    def __init__(self, prev):
 
-        super().__init__(prev.master)
-        atlas_name = prev.atlas_name.get()
-        target_address = prev.target_file_name.get()
-
-        self.header = 'Select slice and estimate rotations using sliders.'
-        self.atlas = Atlas(atlas_name)
-        self.target = Target(target_address, self.atlas)
-
-        
-        # rotation scales
-        self.rot_scales = ttk.Frame(self.frame)
-        self.zrot_scale = ttk.Scale(self.rot_scales, from_=180, to=-180, 
-                                   orient='vertical', length=500,
-                                   variable=self.atlas.thetas[0],
-                                   command=self.update) 
-        self.yrot_scale = ttk.Scale(self.rot_scales, from_=180, to=-180, 
-                                   orient='vertical', length=500,
-                                   variable=self.atlas.thetas[1],
-                                   command=self.update) 
-        self.xrot_scale = ttk.Scale(self.rot_scales, from_=180, to=-180, 
-                                   orient='vertical', length=500,
-                                   variable=self.atlas.thetas[2],
-                                   command=self.update)
-        self.zrot_scale.pack(side=tk.LEFT)
-        self.yrot_scale.pack(side=tk.LEFT)
-        self.xrot_scale.pack(side=tk.LEFT)
-
-        #slice scale
-        self.slice_scale = ttk.Scale(self.frame, from_=self.atlas.pix_loc[0][0], 
-                                     to=self.atlas.pix_loc[0][-1],
-                                     orient='horizontal', length=500, 
-                                     variable=self.atlas.curr_slice,
-                                     command=self.update)
-
-        # show images
-        self.create_figure(1,2)
-        self.fig.axes[1].imshow(self.target.get_img())
-        self.update()
-
-        self.slice_scale.grid(row=0, column=1)
-        self.rot_scales.grid(row=1, column=0)
-        self.canvas.get_tk_widget().grid(row=1, column=1)
-        self.toolbar_frame.grid(row=2, column=1)
-
-    def update(self, _=None):
-        self.atlas.set_LT() # set L and T based on scales
-        self.fig.axes[0].cla()
-        self.fig.axes[0].imshow(self.atlas.get_img(False,True)) # get img, without segmentation and quickly
-        self.fig.canvas.draw_idle()
-    
-    def next(self):
-        self.deactivate()
-        return Landmark_Annotator(self)
-
-class Landmark_Annotator(Page):
-
-    def __init__(self, prev):
-        super().__init__(prev.master)
-
-        # controls
-        self.header = '''Mark atlas-target point pairs one pair at a time and submit each pair before marking the next! Click 'Next' to move on
-        
-        Controls:
-        L-click\tmark point
-        R-click\tremove point
-        Enter\tsubmit point
-        Backspace\tdelete last submitted
-        Dbl L-click\ttoggle segmentation'''
-
-        self.atlas = prev.atlas
-        self.target = prev.target
-
-        # Get slice images without and with segmentation, full quality (quickReturn=False)
-        self.slice_img = self.atlas.get_img(False, False)
-        self.slice_seg = self.atlas.get_img(True, False)
-        self.show_seg = False
-
-        self.imgs = [self.slice_img, self.target.get_img()]
-        self.extents = [self.atlas.get_extent(), self.target.get_extent()]
-        self.points = [ [], [] ] # landmark points, points[0][i] in atlas corresponds with points[1][i] in target
-        self.new_pt = [ [], [] ]
-        self.pt_sz = 4
-    
-        #show images
-        self.create_figure(1,2)
-        self.update()
-
-        self.canvas.get_tk_widget().grid(row=0, column=0)
-        self.toolbar_frame.grid(row=1, column=0)
-
-        self.canvas.mpl_connect('button_press_event', self.onclick)
-        self.canvas.mpl_connect('key_press_event',self.onpress)
-        self.canvas.get_tk_widget().bind('<Double-1>',self.ondouble)
-
-
-    def update(self, axis=None):
-
-        if axis is None:
-            self.update(0)
-            self.update(1)
-            return
-        elif axis not in [0,1]: 
-            raise Exception(f"Bad call to Landmark_Annotator.update(axis={axis})")
-
-        self.fig.axes[axis].cla()
-        self.fig.axes[axis].imshow(self.imgs[axis], extent=self.extents[axis])
-        
-        # plot prev selected points in white
-        if len(self.points[0]): 
-            self.fig.axes[axis].scatter(np.array(self.points)[axis,:,1],
-                                        np.array(self.points)[axis,:,0], 
-                                        color='white', s=self.pt_sz)
-        # plot current new point in red
-        if len(self.new_pt[0]):
-            print(self.new_pt)
-            self.fig.axes[axis].scatter(self.new_pt[axis][1],self.new_pt[axis][0], 
-                                        color='red', s=self.pt_sz)
-
-
-        self.canvas.draw()
-    
-    def ondouble(self, event):
-        self.show_seg = not self.show_seg # toggle show_seg
-        if self.show_seg: 
-            self.imgs[0] = self.slice_seg
-            print("showing segmentation")
-        else: 
-            self.imgs[0] = self.slice_img
-            print("hiding segmentation")
-        self.update(0)
-
-    def onclick(self, event):
-        if event.xdata == None: return # clicked outside of axes
-        ix, iy = int(event.xdata), int(event.ydata) # get x and y data of pt
-        msg = ''
-        
-        axis = -1
-        # based on where user clicked, set axis and update output msg
-        if event.inaxes == self.fig.axes[0]: # user clicked atlas
-            axis = 0
-            msg = "source at " + msg
-        elif event.inaxes == self.fig.axes[1]: # user clicked target
-            axis = 1
-            msg = "target at " + msg
-        else: print("ERROR: clicked outside axes")
-    
-        if event.button == 1: # left click means add point at mouse location
-            #self.fig.axes[axis].scatter([ix],[iy], color='red', s=self.pt_sz)
-            self.new_pt[axis] = [iy, ix]
-            msg = 'point added to ' + msg + f'[x,y]=[{ix},{iy}]'
-        elif event.button == 3: # right click means remove previously created point
-            msg = 'point removed from ' + msg + f'[x,y]={self.new_pt[axis][::-1]}'
-            self.new_pt[axis] = []
-
-        self.update(axis)
-        print(msg)
-
-    def onpress(self, event):
-        if event.key == 'enter': # enter key used to commit selected points to points list
-
-            if not len(self.new_pt[0])*len(self.new_pt[1]): # if missing a point in either axis, throw error
-                print("ERROR: attempted landmark save with one or more points missing!")
-                return
-
-            # add new points to list, notify user, and clear out new points list
-            self.points[0].append(self.new_pt[0])
-            self.points[1].append(self.new_pt[1])
-            print(f"Added {self.new_pt[0][::-1]} and {self.new_pt[1][::-1]} to points list")
-            self.new_pt[0] = []
-            self.new_pt[1] = []
-            self.update()
-        
-        if event.key == 'backspace': # backspace key used to remove recently committed point
-            if len(self.points[0]) == 0: return # if no points to remove, simply return
-            print(f'Removed [{self.points[0][-1][::-1]}] and [{self.points[1][-1][::-1]}]') # user msg
-            
-            # remove last pair of poins
-            self.points[0].pop(-1)
-            self.points[1].pop(-1)
-            self.update() # refresh both axes
-
-    def next(self):
-        self.deactivate()
-        return STalign_Runner(self)
-    
 class STalign_Runner(Page):
 
     def __init__(self, prev):

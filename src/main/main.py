5ba1304fde3ab7b32f2c154de3d6185158c71fcd
Rishi <rishikoneru2005@gmail.com>
2025-04-17 22:49:15 -0400

    completed exporter

--- src/main/main.py
+++ src/main/main.py
@@ -27,7 +27,8 @@ class App(tk.Tk):
             TargetProcessor,
             STalignRunner,
             VisuAlignRunner,
-            RegionPicker
+            RegionPicker,
+            Exporter
         ])
         self.pages: list[Page] = [page(self.main_window, self.slides, self.atlases) for page in page_list] # initalize each page in here with self.main_window as parent
         self.page_index = 0
